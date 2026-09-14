e91275bba11e0b25329fd1994d465a1ae0604f10
DotacjaPlus <dawid.sobol2002@gmail.com>
2026-09-14 02:32:58 +0200

    Register production FlowPilot promo composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import {Composition} from 'remotion';
+import {FlowPilotPromo} from './FlowPilotPromo';
 import {Reel} from './Reel';
 
 export const RemotionRoot: React.FC = () => {
   return (
-    <Composition
-      id="Reel"
-      component={Reel}
-      durationInFrames={450}
-      fps={30}
-      width={1080}
-      height={1920}
-      defaultProps={{
-        title: 'FlowPilot AI',
-        subtitle: 'Automatyzuj obsługę klientów i odzyskaj czas',
-        cta: 'Zobacz, jak to działa',
-      }}
-    />
+    <>
+      <Composition
+        id="FlowPilotPromo"
+        component={FlowPilotPromo}
+        durationInFrames={450}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+
+      <Composition
+        id="Reel"
+        component={Reel}
+        durationInFrames={450}
+        fps={30}
+        width={1080}
+        height={1920}
+        defaultProps={{
+          title: 'FlowPilot AI',
+          subtitle: 'Automatyzuj obsługę klientów i odzyskaj czas',
+          cta: 'Zobacz, jak to działa',
+        }}
+      />
+    </>
   );
 };
