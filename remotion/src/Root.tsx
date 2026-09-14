6fa2f74880c1caea415e1e2b13a7b279e131a126
DotacjaPlus <dawid.sobol2002@gmail.com>
2026-09-14 02:44:20 +0200

    Register final 20s FlowPilot ad composition

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {Composition} from 'remotion';
+import {FlowPilotAdFinal} from './FlowPilotAdFinal';
 import {FlowPilotPromo} from './FlowPilotPromo';
 import {Reel} from './Reel';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
+      <Composition
+        id="FlowPilotAdFinal"
+        component={FlowPilotAdFinal}
+        durationInFrames={600}
+        fps={30}
+        width={1080}
+        height={1920}
+      />
+
       <Composition
         id="FlowPilotPromo"
         component={FlowPilotPromo}
